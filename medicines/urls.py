adcc1d0cb0f7f5ad07dfd2ba5a50de152da34b89
dain <mong2822@naver.com>
2025-12-17 22:15:23 +0900

    feat : review, comment, score CRUD 구현

--- medicines/urls.py
+++ medicines/urls.py
@@ -4,4 +4,13 @@ from . import views
 
 urlpatterns = [
     path('drugs/', views.drug_list),
+    # 리뷰 CRUD
+    path('drugs/<int:drug_pk>/reviews/', views.review_list),    # GET(전체), POST
+    path('drugs/<int:drug_pk>/reviews/<int:review_pk>/', views.review_detail),  # GET(상세), PUT, DELETE
+    # 댓글 CRUD
+    path('reviews/<int:review_pk>/comments/', views.comment_list),  # GET, POST
+    path('reviews/<int:review_pk>/comments/<int:comment_pk>/', views.comment_detail),   # PUT, DELETE
+    # 별점 CRUD
+    path('drugs/<int:drug_pk>/scores/', views.score_list),  # GET, POST
+    path('drugs/<int:drug_pk>/scores/<int:score_pk>/', views.score_detail), # PUT, DELETE
 ]
