1234d9ced8dbeb015d8893aa8f20bec2c459d36a
parkmiyeong <parkmiyoung2175@gmail.com>
2025-12-17 22:21:14 +0900

    feat : frontend 뼈대 구축

--- drugsafe_backend/drugsafe/urls.py
+++ drugsafe_backend/drugsafe/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('medicines.urls')),
+    path('medicines/', include('medicines.urls')),
 ]
